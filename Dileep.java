2c5d80bf9fa11d9abf5da38ccef1f174efbe79ba
Ammu-82778 <ammuammu82778@gmail.com>
2023-05-05 22:22:42 +0530

    first

---
+++ Dileep.java
@@ -0,0 +1,15 @@
+class Dileep{
+    public static void main(String []args){
+        int i=5;
+        Integer i2=new Integer(50);
+        system.out.println("creating"+i);
+        system.out.println("obtainting"+i2);
+        Character gfg=new Character('a');
+        char a1=gfg;
+        system.out.println("creating"+gfg);
+        system.out.println("obtainting"+a1);
+    System.out.println();
+ 
+    }
+    }
+}
